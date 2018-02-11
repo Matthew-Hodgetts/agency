3b85a008384b504bdd8ec329cd119969c79160f4
Matthew Hodgetts <matt.hodgetts@outlook.com>
2018-02-11 21:29:31 +0000

    Boilerplate ready

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -1,7 +1,7 @@
 <?php
 function wordpressify_resources() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-	wp_enqueue_script( 'header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false );
+	//wp_enqueue_script( 'header_js', get_template_directory_uri() . '/js/header-bundle.js', null, 1.0, false );
 	wp_enqueue_script( 'footer_js', get_template_directory_uri() . '/js/footer-bundle.js', null, 1.0, true );
 }
 
